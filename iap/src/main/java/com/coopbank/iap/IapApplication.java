4e892a5b9a1ed0ce5bb6219dd7a22c828e80fafc
Yohannes <jonmersha@gmail.com>
2021-10-01 09:40:11 +0300

    first commit

---
+++ iap/src/main/java/com/coopbank/iap/IapApplication.java
@@ -0,0 +1,13 @@
+package com.coopbank.iap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IapApplication.class, args);
+	}
+
+}
---
+++ iap/src/test/java/com/coopbank/iap/IapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coopbank.iap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
